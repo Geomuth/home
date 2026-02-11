8c86786ed21d6838042542bcad78e55525ee1fa0
geoffrey muthoka <geoffreymuthoka200@gmail.com>
2026-02-11 22:54:39 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -10,7 +10,7 @@ module.exports = async (req, res) => {
     if (req.method === 'OPTIONS') {
         res.status(200).end();
         return;
-    }
+    } 
  
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
