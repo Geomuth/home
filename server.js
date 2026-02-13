0a663e1cf850d184175e9aeee4fb53cc98225a87
geoffrey muthoka <geoffreymuthoka200@gmail.com>
2026-02-13 17:09:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -41,27 +41,13 @@ function addUnmatchedQuestion(question) {
 
 app.use(cors());
 app.use(express.json());
+
+// Serve all static files (CSS, JS, HTML, JSON)
 app.use(express.static(path.join(__dirname, '.')));
-
-// Explicitly serve CSS with proper headers
-app.get('/style.css', (req, res) => {
-  res.type('text/css');
-  res.sendFile(path.join(__dirname, 'style.css'));
-});
-
-// Explicitly serve JS files
-app.get('/app.js', (req, res) => {
-  res.type('application/javascript');
-  res.sendFile(path.join(__dirname, 'app.js'));
-});
-
-app.get('/chat.js', (req, res) => {
-  res.type('application/javascript');
-  res.sendFile(path.join(__dirname, 'chat.js'));
-});
 
 // Root
 app.get('/', (req, res) => {
+  res.type('text/html');
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
